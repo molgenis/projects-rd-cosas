3488a28279970dfdd8e5b79d6738e9fc31ec1f9a
davidruvolo51 <davidruvolo51@gmail.com>
2021-04-09 17:07:31 +0200

    regenerated emx

--- R/cosas_01_main.R
+++ R/cosas_01_main.R
@@ -2,7 +2,7 @@
 #' FILE: cosas_01_main.R
 #' AUTHOR: David Ruvolo
 #' CREATED: 2021-03-16
-#' MODIFIED: 2021-03-16
+#' MODIFIED: 2021-04-09
 #' PURPOSE: convert main yml into EMX
 #' STATUS: working
 #' PACKAGES: dplyr
@@ -34,7 +34,11 @@ invisible(
 
 #' ~ 1 ~
 #' convert and write
-model <- yml_to_emx(path = "data/cosas/cosas-prototype.yml")
-model$attributes <- model$attributes %>% dplyr::rename(`label-nl` = label.nl)
+model <- yml_to_emx(path = "data/_emx/cosas.yml")
+model$attributes <- model$attributes %>%
+    dplyr::rename(
+        `label-nl` = label.nl,
+        `description-nl` = description.nl
+    )
 model$packages <- model$packages %>% dplyr::rename(id = name)
 write_emx(model = model, out_dir = "data/cosas")
