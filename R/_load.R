789d2f0537d620be1c6180bb0c2fe74b5f51b0a5
davidruvolo51 <davidruvolo51@gmail.com>
2021-08-16 17:24:58 +0200

    updated import rules

--- R/_load.R
+++ R/_load.R
@@ -2,7 +2,7 @@
 #' FILE: _load.R
 #' AUTHOR: David Ruvolo
 #' CREATED: 2021-07-29
-#' MODIFIED: 2021-07-29
+#' MODIFIED: 2021-08-16
 #' PURPOSE: load and combine portal files (if desired)
 #' STATUS: working
 #' PACKAGES: readxl, openxlsx
@@ -15,7 +15,7 @@ cli::cli_alert_info("Loading portal files...")
 portal_patients <- readxl::read_xlsx(
     path = "_raw/cosasportal_patients.xlsx",
     sheet = 1,
-    col_types = c("text", "date", "text", "date", rep("text", 4)),
+    col_types = c("text", "date", "text", "date", rep("text", 4))
 )
 
 portal_diagnoses <- readxl::read_xlsx(
@@ -33,19 +33,19 @@ portal_samples <- readxl::read_xlsx(
 portal_array_adlas <- readxl::read_xlsx(
     path = "_raw/cosasportal_array_adlas.xlsx",
     sheet = 1,
-    col_types = c(rep("text", 29))
+    col_types = "text"
 )
 
 portal_array_darwin <- readxl::read_xlsx(
     path = "_raw/cosasportal_array_darwin.xlsx",
     sheet = 1,
-    col_types = c(rep("text", 2), "date", rep("text", 4))
+    col_types = c("text", "text", "date", rep("text", 4))
 )
 
 portal_ngs_adlas <- readxl::read_xlsx(
     path = "_raw/cosasportal_ngs_adlas.xlsx",
     sheet = 1,
-    col_types = c(rep("text", 14))
+    col_types = "text"
 )
 
 portal_ngs_darwin <- readxl::read_xlsx(
