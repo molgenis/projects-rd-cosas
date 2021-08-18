f9cdd0ff2ac2f6861fa61efb61c60b367d77a891
davidruvolo51 <davidruvolo51@gmail.com>
2021-08-18 17:19:31 +0200

    added mappings for patients, clinical, and samples

--- R/mapping_cosas.R
+++ R/mapping_cosas.R
@@ -2,13 +2,14 @@
 #' FILE: cosas_mapping.R
 #' AUTHOR: David Ruvolo
 #' CREATED: 2021-07-22
-#' MODIFIED: 2021-08-17
+#' MODIFIED: 2021-08-18
 #' PURPOSE: Mapping portal tables to main pkg
 #' STATUS: working
-#' PACKAGES: data.table, purrr
+#' PACKAGES: data.table, purrr, dplyr, tidyr
 #' COMMENTS: NA
 #' ////////////////////////////////////////////////////////////////////////////
 
+library2("data.table")
 source("R/_load.R")
 source("R/utils_mapping.R")
 
@@ -24,96 +25,212 @@ cosas_ngs_adlas_mapped <- mappings$ngs_adlas(portal_ngs_adlas)
 cosas_ngs_darwin_mapped <- mappings$ngs_darwin(portal_ngs_darwin)
 cosas_bench_cnv_mapped <- mappings$bench_cnv(portal_bench_cnv)
 
+rm(list = c(
+    "portal_patients", "portal_diagnoses", "portal_samples",
+    "portal_array_adlas", "portal_array_darwin", "portal_ngs_adlas",
+    "portal_ngs_darwin", "portal_bench_cnv"
+))
+
 #'//////////////////////////////////////
 
 
-#### ==== PICK UP HERE ==== ####
+cli::cli_alert_info("Building COSAS tables...")
 
-# prepare bench_cnv dataset
-# pull cases that exist in the main patients tables, and then split by
-# fetus status. This is important as fetus records will be binded to
-# `cosas_patients` as new rows. Both fetus and non-fetus cases will be added
-# to `cosas_clinical`.
-cli::cli_alert_info("Filtering {.val benchCNV} and calcuating earliest date")
-cosas_fetuses <- cosas_bench_cnv_mapped %>%
-    filter(family_numr %in% cosas_patients_mapped$family_numr, is_fetus) %>%
-    # this will get rid of the IDs with multiple HPO entries
-    # this information will be added later in the clinical table
-    distinct(
-        umcg_numr, family_numr, biological_sex, maternal_id,
-        is_fetus, is_twin, linked_patient_id
-    ) %>%
-    # calculate and join earliest date seen
-    left_join(
-        cosas_bench_cnv_mapped %>%
-            select(umcg_numr, date_created) %>%
-            filter(!is.na(date_created)) %>%
-            group_by(umcg_numr) %>%
-            mutate(
-                date_first_consult = lubridate::ymd(date_created)
-            ) %>%
-            summarize(
-                date_first_consult = min(date_first_consult, na.rm = TRUE)
-            ),
-        by = "umcg_numr"
-    ) %>%
-    distinct(umcg_numr, .keep_all = TRUE) # remove duplicate entries
 
-# bind fetus cases to main patients
-cli::cli_alert_info("Combining patients and fetus data...")
-cosas_patients_fetuses <- cosas_patients_mapped %>%
-    bind_rows(cosas_fetuses) %>%
-    arrange(umcg_numr)
+#' @title Create COSAS Patients
+#' @description create `cosas_patients`
+#' @section Methodology:
+#'
+#' `cosas_patients` is created from `cosas_patients_mapped` and
+#' `cosas_bench_cnv_mapped`. This object is created by binding new records
+#' from bench_cnv and joining metadata for existing records (i.e., patients).
+#'
+#' Using the mapping CNV dataset, find records that do not exist in
+#' `cosas_patients_mapped`. Grab metadata for unique cases and bind them
+#' to the patients dataset.
+#'
+#' For records that do exist, merge metadata into patients. Since most metadata
+#' is present in the data extracts and applies to records that are fetuses,
+#' merge `fetusStatus` and `twinStatus`. These will always be FALSE, but it is
+#' import to merge this data anyways.
+#'
+#' It is not possible to reliably -- or accurately -- calculate date of first
+#' consult using dateCreated or other data columns. If this is required, this
+#' information should be requested.
+#'
+#' @noRd
 
-# code to check for multiple entries per fetus
-# cosas_bench_cnv_mapped %>%
-#     filter(
-#         family_numr %in% cosas_patients_mapped$family_numr,
-#         is_fetus
-#     ) %>%
-#     group_by(umcg_numr) %>%
-#     summarize(
-#         count = length(umcg_numr)
-#     ) %>%
-#     arrange(-count)
+# isolate new cases
+new_cosas_cases <- cosas_bench_cnv_mapped[
+    !umcgID %in% cosas_patients_mapped$umcgID & !duplicated(umcgID),
+    .(
+        umcgID,
+        familyID,
+        biologicalSex,
+        maternalID,
+        fetusStatus,
+        twinStatus,
+        altPatientID
+    )
+]
 
-#' //////////////////////////////////////
+# isolate existing cases
+existing_cosas_cases <- cosas_bench_cnv_mapped[
+    umcgID %in% cosas_patients_mapped$umcgID & !duplicated(umcgID),
+    .(umcgID, fetusStatus, twinStatus)
+]
 
-#' ~ 4 ~
-#' Merge Data
+# rbind objects and join data of existing cases
+cosas_patients_base <- data.table::rbindlist(
+    list(cosas_patients_mapped, new_cosas_cases),
+    fill = TRUE
+)
 
-cosas_samples_mapped %>%
-    select(umcg_numr, request_id, sample_id, dna_numr, material_type, test_code) %>%
-    left_join(
-        cosas_array_adlas_mapped,
-        by = c("umcg_numr", "request_id", "dna_numr", "test_code")
+cosas_patients <- merge(
+    x = cosas_patients_base,
+    y = existing_cosas_cases,
+    by = "umcgID",
+    all.x = TRUE
+)
+
+# unite fetusStatus and twinStatus columns and set dateLastUpdated
+cosas_patients[, `:=`(
+    fetusStatus = paste(fetusStatus.x, fetusStatus.y, sep = "="),
+    twinStatus = paste(twinStatus.x, twinStatus.y, sep = "="),
+    fetusStatus.x = NULL,
+    fetusStatus.y = NULL,
+    twinStatus.x = NULL,
+    twinStatus.y = NULL
+)][, `:=`(
+    fetusStatus = gsub("=NA", "", fetusStatus),
+    twinStatus = gsub("=NA", "", twinStatus),
+    dateLastUpdated = utils$timestamp()
+)]
+
+
+rm(list = c("new_cosas_cases", "existing_cosas_cases", "cosas_patients_base"))
+
+
+# create object containing only familyIds by umcgId
+patientFamilyIDs <- cosas_patients[, .(umcgID, familyID)]
+
+#' @title Create Cosas Clinical Events
+#' @description create `cosas_clinical`
+#' @section Methodology:
+#'
+#' All diagnostic information is stored in `cosas_clinical`. The purpose is
+#' to preserve the structure of the diagnostic data export. This dataset is
+#' structured in long format. This means that each row is a unique diagnosis
+#' per patient. It is important to preserve this structure as all diagnoses
+#' are also given a certainty rating.
+#'
+#' In order to build the dataset, all rows that were missing values across
+#' all columns (expected for umcgID) were removed. A subset of the benchCNV
+#' dataset containing the phenotypic data was created, and then bound
+#' to the initial clinical data.table. Lastly, familyID was merged from
+#' `cosas_patients` and timestamps were generated.
+#'
+#' @noRd
+#'
+
+# create base `cosas_clinical` data.table
+cosas_clinical_base <- cosas_diagnoses_mapped[,
+    isNA := is.na(clinicalDiagnosis) &
+    is.na(clinicalDiagnosisCertainty) &
+    is.na(secondaryDiagnosis) &
+    is.na(secondaryDiagnosisCertainty) &
+    is.na(dateOfDiagnosis) &
+    is.na(ondID)
+][isNA %like% FALSE][, `:=`(isNA = NULL, observedPhenotype = NA_character_)]
+
+
+# gather observedPhenotype and dateOfDiagnosis from benchCNV
+new_clinical_data <- cosas_bench_cnv_mapped[
+    , .(umcgID, observedPhenotype, dateOfDiagnosis)
+][
+    , isNA := is.na(observedPhenotype) & is.na(dateOfDiagnosis)
+][isNA %like% FALSE][, isNA := NULL]
+
+
+# bind tables
+cosas_clinical_bound <- data.table::rbindlist(
+    l = list(cosas_clinical_base, new_clinical_data),
+    fill = TRUE
+)
+
+# join familyID
+cosas_clinical <- merge(
+    x = cosas_clinical_bound,
+    y = cosas_patients[, .(umcgID, familyID)],
+    by = "umcgID",
+    all.x = TRUE
+)[, dateLastUpdated := utils$timestamp()]
+
+
+rm(list = c("cosas_clinical_base", "new_clinical_data", "cosas_clinical_bound"))
+
+
+#' @title Create Cosas Samples
+#' @description create `cosas_samples`
+#' @section Methodology:
+#'
+#' The table `cosas_samples` is created using a some data from the experiment
+#' exports. The samples metadata export is already in good shape. There isn't
+#' much that we need to add. The following code block merges the attributes
+#' `familyID` from `cosas_patients` and creates the column `dateLastUpdated`,
+#' as well as adds `labIndication` and `testDate` where applicable (from the
+#' Darwin exports).
+#'
+#' `testID` is no longer included in this dataset as `testCode` is widely used
+#' in the data export and `testID` is not always available.
+#'
+#' I would like to eventually convert the columns `labResultAvailability` and
+#' `authorizedStatus` into a reference entity. However, I would like to
+#' align with FairGenomes.
+#'
+#' @noRd
+cosas_samples <- cosas_samples_mapped %>%
+    left_join(cosas_patients[, .(umcgID, familyID)], by = "umcgID") %>%
+    left_join(cosas_array_darwin_mapped, by = c("umcgID", "testCode")) %>%
+    mutate(
+        dateLastUpdated = utils$timestamp()
     )
 
 
-cosas_array_adlas_mapped %>%
-    # merge samples metadata
-    left_join(
-        cosas_samples_mapped %>%
-            select(
-                umcg_numr, request_id, sample_id,
-                dna_numr, material_type, test_code
-            ),
-        by = c("umcg_numr", "request_id", "dna_numr", "test_code")
-    ) %>%
-    # merge array data from darwin
-    left_join(
-        cosas_array_darwin_mapped,
-        by = c("umcg_numr", "test_code")
-    ) %>%
-    # merge patient metadata
-    left_join(
-        cosas_patients_fetuses %>%
-            select(umcg_numr, family_numr),
-        by = "umcg_numr"
-    ) %>%
-    mutate(date_last_updated = utils$timestamp()) %>%
-    select(umcg_numr, family_numr, everything()) %>%
-    arrange(umcg_numr)
+# join familyIDs
+cosas_samples_base <- merge(
+    x = cosas_samples_mapped,
+    y = patientFamilyIDs,
+    by = "umcgID",
+    all.x = TRUE
+)
+
+# pull testDate and labIndication from Darwin extracts
+lab_metadata <- data.table::rbindlist(
+    list(
+        cosas_array_darwin_mapped[, .(umcgID, testCode, testDate, labIndication)],
+        cosas_ngs_darwin_mapped[, .(umcgID, testCode, testDate, labIndication)]
+    )
+)
+
+
+# merge lab metadata
+cosas_samples <- merge(
+    x = cosas_samples_base,
+    y = lab_metadata,
+    by = c("umcgID", "testCode"),
+    all.x = TRUE
+)
+
+# add dateLastUpdated
+cosas_samples[, dateLastUpdated := utils$timestamp()]
+
+
+#### =========== PICK UP HERE =========== ####
+
+
+#' @title Create Cosas Labs Array
+
 
 
 # create: `cosas_labs_array`
@@ -155,131 +272,6 @@ cosas_labs_ngs <- cosas_ngs_adlas_mapped %>%
     arrange(umcg_numr)
 
 
-# merge: earliest `date_first_consult` per patient into patients
-cli::cli_alert_info("Building cosas_patients...")
-cosas_patients <- cosas_patients_fetuses %>%
-    left_join(
-        cosas_diagnoses_mapped %>%
-            select(umcg_numr, date_first_consult) %>%
-            filter(
-                !is.na(date_first_consult),
-                umcg_numr %in% cosas_patients_fetuses$umcg_numr
-            ) %>%
-            group_by(umcg_numr) %>%
-            mutate(
-                date_first_consult = lubridate::ymd(date_first_consult)
-            ) %>%
-            summarize(
-                date_first_consult = min(date_first_consult, na.rm = TRUE)
-            ),
-        by = "umcg_numr"
-    ) %>%
-    tidyr::unite(
-        data = .,
-        col = "date_first_consult",
-        c("date_first_consult.x", "date_first_consult.y"),
-        na.rm = TRUE
-    ) %>%
-    mutate(
-        date_first_consult = na_if(date_first_consult, ""),
-        date_last_updated = utils$timestamp()
-    )
-
-
-# merge: `family_numr` from patients into samples
-# merge: `test_id`, `lab_indication`, `test_date` from labs_* into samples
-cli::cli_alert_info("Building cosas_samples...")
-cosas_samples <- cosas_samples_mapped %>%
-    left_join(
-        cosas_patients %>%
-            distinct(umcg_numr, family_numr),
-        by = "umcg_numr"
-    ) %>%
-    left_join(
-        cosas_labs_array %>%
-            select(-family_numr),
-        by = c("umcg_numr", "request_id", "dna_numr", "test_code")
-    ) %>%
-    left_join(
-        cosas_labs_ngs %>%
-            select(
-                umcg_numr, request_id, dna_numr,
-                test_id, test_code, test_date,
-                lab_indication
-            ),
-        by = c("umcg_numr", "request_id", "dna_numr", "test_code")
-    ) %>%
-    tidyr::unite(
-        "lab_indication",
-        c("lab_indication.x", "lab_indication.y"),
-        na.rm = TRUE
-    ) %>%
-    tidyr::unite("test_id", c("test_id.x", "test_id.y"), na.rm = TRUE) %>%
-    tidyr::unite("test_date", c("test_date.x", "test_date.y"), na.rm = TRUE) %>%
-    mutate(
-        lab_indication = na_if(lab_indication, ""),
-        test_id = na_if(test_id, ""),
-        test_date = na_if(test_date, ""),
-        date_last_updated = utils$timestamp()
-    ) %>%
-    arrange(umcg_numr, sample_id)
-
-# create: `cosas_clinical`
-cli::cli_alert_info("Building cosas_clinical...")
-cosas_clinical <- cosas_diagnoses_mapped %>%
-    rename(date = date_first_consult) %>%
-    mutate(
-        keep = case_when(
-            is.na(primary_dx) &
-            is.na(primary_dx_certainty) &
-            is.na(extra_dx) &
-            is.na(extra_dx_certainty) &
-            is.na(date) &
-            is.na(ond_id) ~ FALSE,
-            TRUE ~ TRUE
-        )
-    ) %>%
-    filter(keep) %>%
-    select(-keep) %>%
-    mutate(
-        event_type = case_when(
-            is.na(primary_dx) & is.na(extra_dx) & !is.na(ond_id) ~ "OND",
-            !is.na(primary_dx) ~ "DX",
-            TRUE ~ NA_character_
-        )
-    ) %>%
-    arrange(umcg_numr) %>%
-    bind_rows(
-        cosas_bench_cnv_mapped %>%
-            select(umcg_numr, hpo, date = date_created) %>%
-            filter(umcg_numr %in% cosas_patients$umcg_numr) %>%
-            mutate(
-                date = as.character(date),
-                event_type = "HPO"
-            )
-    ) %>%
-    left_join(
-        cosas_patients_fetuses %>%
-            select(umcg_numr, family_numr) %>%
-            filter(umcg_numr %in% cosas_diagnoses_mapped$umcg_numr),
-        by = "umcg_numr"
-    ) %>%
-    select(
-        umcg_numr,
-        family_numr,
-        date,
-        event_type,
-        primary_dx,
-        primary_dx_certainty,
-        extra_dx,
-        extra_dx_certainty,
-        hpo,
-        ond_id
-    ) %>%
-    arrange(umcg_numr) %>%
-    mutate(
-        date_last_updated = utils$timestamp()
-    )
 
 # write files
 cli::cli_alert_info("Saving data to file...")
