48df343302e6ae572c353145705e5bce6f218a76
davidruvolo51 <davidruvolo51@gmail.com>
2021-08-30 12:40:00 +0200

    removed cli messages

--- R/_load.R
+++ R/_load.R
@@ -2,15 +2,13 @@
 #' FILE: _load.R
 #' AUTHOR: David Ruvolo
 #' CREATED: 2021-07-29
-#' MODIFIED: 2021-08-17
+#' MODIFIED: 2021-08-30
 #' PURPOSE: load and combine portal files (if desired)
 #' STATUS: working
 #' PACKAGES: readxl, openxlsx
 #' COMMENTS: Use this script to consistently load all files across scripts
 #'      adjust the write argument if needed
 #'////////////////////////////////////////////////////////////////////////////
-
-cli::cli_alert_info("Loading portal files...")
 
 portal_patients <- readxl::read_xlsx(
     path = "_raw/cosasportal_patients.xlsx",
@@ -64,19 +62,3 @@ portal_bench_cnv <- readxl::read_xlsx(
     sheet = 1,
     col_types = c(rep("text", 6), "date")
 )
-
-
-# check
-if (
-    !is.null(portal_patients) &
-    !is.null(portal_diagnoses) &
-    !is.null(portal_samples) &
-    !is.null(portal_array_adlas) &
-    !is.null(portal_array_darwin) &
-    !is.null(portal_ngs_adlas) &
-    !is.null(portal_ngs_darwin)
-) {
-    cli::cli_alert_success("Successfully loaded files! :-) ")
-} else {
-    cli::cli_alert_danger("Failed to load files :-(")
-}
