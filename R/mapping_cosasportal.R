a00b8533854cadb84661cf5220ab626ba41000f2
davidruvolo51 <davidruvolo51@gmail.com>
2021-08-30 11:58:05 +0200

    removed magittr

--- R/mapping_cosasportal.R
+++ R/mapping_cosasportal.R
@@ -2,12 +2,11 @@
 #' FILE: mapping_cosasportal.R
 #' AUTHOR: David Ruvolo
 #' CREATED: 2021-07-28
-#' MODIFIED: 2021-07-29
+#' MODIFIED: 2021-08-30
 #' PURPOSE: load and combine portal files (if desired)
 #' STATUS: working
-#' PACKAGES: readxl, openxlsx
-#' COMMENTS: Use this script to consistently load all files across scripts
-#'      adjust the write argument if needed
+#' PACKAGES: openxlsx
+#' COMMENTS: NA
 #'////////////////////////////////////////////////////////////////////////////
 
 # source data
@@ -15,29 +14,24 @@ source("R/_load.R")
 
 # write `cosasportal`
 cli::cli_alert_info("Writing portal data to file...")
-openxlsx::createWorkbook() %T>%
-    openxlsx::addWorksheet(., "cosasportal_labs_array_adlas") %T>%
-    openxlsx::addWorksheet(., "cosasportal_labs_array_darwin") %T>%
-    openxlsx::addWorksheet(., "cosasportal_bench_cnv") %T>%
-    openxlsx::addWorksheet(., "cosasportal_diagnoses") %T>%
-    openxlsx::addWorksheet(., "cosasportal_labs_ngs_adlas") %T>%
-    openxlsx::addWorksheet(., "cosasportal_labs_ngs_darwin") %T>%
-    openxlsx::addWorksheet(., "cosasportal_patients") %T>%
-    openxlsx::addWorksheet(., "cosasportal_samples") %T>%
-    openxlsx::writeData(
-        ., "cosasportal_labs_array_adlas", portal_array_adlas
-    ) %T>%
-    openxlsx::writeData(
-        ., "cosasportal_labs_array_darwin", portal_array_darwin
-    ) %T>%
-    openxlsx::writeData(., "cosasportal_bench_cnv", portal_bench_cnv) %T>%
-    openxlsx::writeData(., "cosasportal_diagnoses", portal_diagnoses) %T>%
-    openxlsx::writeData(
-        ., "cosasportal_labs_ngs_adlas", portal_ngs_adlas
-    ) %T>%
-    openxlsx::writeData(
-        ., "cosasportal_labs_ngs_darwin", portal_ngs_darwin
-    ) %T>%
-    openxlsx::writeData(., "cosasportal_patients", portal_patients) %T>%
-    openxlsx::writeData(., "cosasportal_samples", portal_samples) %T>%
-    openxlsx::saveWorkbook(., "data/cosasportal/cosasportal.xlsx", TRUE)
+wb <- openxlsx::createWorkbook()
+openxlsx::addWorksheet(wb, "cosasportal_labs_array_adlas")
+openxlsx::addWorksheet(wb, "cosasportal_labs_array_darwin")
+openxlsx::addWorksheet(wb, "cosasportal_bench_cnv")
+openxlsx::addWorksheet(wb, "cosasportal_diagnoses")
+openxlsx::addWorksheet(wb, "cosasportal_labs_ngs_adlas")
+openxlsx::addWorksheet(wb, "cosasportal_labs_ngs_darwin")
+openxlsx::addWorksheet(wb, "cosasportal_patients")
+openxlsx::addWorksheet(wb, "cosasportal_samples")
+openxlsx::writeData(wb, "cosasportal_labs_array_adlas", portal_array_adlas)
+openxlsx::writeData(wb, "cosasportal_labs_array_darwin", portal_array_darwin)
+openxlsx::writeData(wb, "cosasportal_bench_cnv", portal_bench_cnv)
+openxlsx::writeData(wb, "cosasportal_diagnoses", portal_diagnoses)
+openxlsx::writeData(wb, "cosasportal_labs_ngs_adlas", portal_ngs_adlas)
+openxlsx::writeData(wb, "cosasportal_labs_ngs_darwin", portal_ngs_darwin)
+openxlsx::writeData(wb, "cosasportal_patients", portal_patients)
+openxlsx::writeData(wb, "cosasportal_samples", portal_samples)
+openxlsx::saveWorkbook(wb, "data/cosasportal/cosasportal.xlsx", TRUE)
+
+# clean up
+rm2()
