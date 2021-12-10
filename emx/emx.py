152bddef4294ca327be308fdc851b0af21242481
davidruvolo51 <davidruvolo51@gmail.com>
2021-12-10 13:09:12 +0100

    reinit portal emx (#12)

--- emx/emx.py
+++ emx/emx.py
@@ -9,17 +9,19 @@
 #' COMMENTS: NA
 #'////////////////////////////////////////////////////////////////////////////
 
-import sys
 from yamlemxconvert import Convert
-# from datatable import fread
-import pandas as pd
+
+# render umdm portal
+umdm = Convert(files = ['emx/src/umdmportal.yaml'])
+umdm.convert()
+umdm.write('umdmportal', format='xlsx', outDir='emx/dist')
+umdm.write_schema(path = 'emx/schemas/umdmportal_schema.md')
+
 
 # Jobs: the main jobs module is located here on GitHub (see 'jobs.xlsx')
 # https://github.com/molgenis/rd-datamodel/tree/main/emx/dist
-jobsModule = Convert(files = ['emx/src/jobs_results.yaml'])
-jobsModule.convert()
-
-# import pandas as pd
-pd.DataFrame(jobsModule.attributes).to_csv('emx/dist/jobs_results_bamdata.csv', index = False)
+# jobsModule = Convert(files = ['emx/src/jobs_results.yaml'])
+# jobsModule.convert()
+# pd.DataFrame(jobsModule.attributes).to_csv('emx/dist/jobs_results_bamdata.csv', index = False)
 
 # mcmd import -p emx/dist/jobs_results_bamdata.csv --as attributes --in jobs_results
