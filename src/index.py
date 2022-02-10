3b857fc9c1613f2fde7cec51d8a314190b230ae2
davidruvolo51 <davidruvolo51@gmail.com>
2022-02-10 13:42:31 +0100

    added test for file import

--- src/index.py
+++ src/index.py
@@ -25,3 +25,20 @@ umdm.write_schema(path = 'emx/schemas/umdmportal_schema.md')
 # pd.DataFrame(jobsModule.attributes).to_csv('emx/dist/jobs_results_bamdata.csv', index = False)
 
 # mcmd import -p emx/dist/jobs_results_bamdata.csv --as attributes --in jobs_results
+
+#//////////////////////////////////////
+
+# ~ 1 ~ 
+# Tests
+
+from src.python.emx2_client import Molgenis
+from dotenv import load_dotenv
+from os import environ
+load_dotenv()
+
+host = environ['EMX2_HOST']
+database = environ['EMX2_DB_PRIMARY']
+
+db = Molgenis(url = host, database = database)
+db.signin(email=environ['EMX2_USERNAME'],password=environ['EMX2_PASSWORD'])
+db.importCSV(table = 'organizations', filename='dist/cosas_organizations.csv')
