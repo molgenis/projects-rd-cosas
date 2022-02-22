396b46a1a6870c4bef030189feab2c42953f1ecc
davidruvolo51 <davidruvolo51@gmail.com>
2022-02-22 09:40:08 +0100

    added update attrib method

--- src/cosas/cosas_mappings.py
+++ src/cosas/cosas_mappings.py
@@ -2,7 +2,7 @@
 #' FILE: mappings_cosas.py
 #' AUTHOR: David Ruvolo
 #' CREATED: 2021-10-05
-#' MODIFIED: 2022-02-17
+#' MODIFIED: 2022-02-22
 #' PURPOSE: primary mapping script for COSAS
 #' STATUS: stable
 #' PACKAGES: **see below**
@@ -11,7 +11,7 @@
 
 from datatable import dt, f, as_type, first
 import molgenis.client as molgenis
-from datetime import datetime, tzinfo
+from datetime import datetime
 import numpy as np
 import requests
 import json
@@ -43,7 +43,6 @@ def status_msg(*args):
     print('\033[94m[' + t + '] \033[0m' + ' '.join(map(str, args)))
 
 
-# extend molgenis.client
 class Molgenis(molgenis.Session):
     def __init__(self, *args, **kwargs):
         super(Molgenis, self).__init__(*args, **kwargs)
@@ -119,11 +118,12 @@ class Molgenis(molgenis.Session):
                 )
     
     
-    def updateData(self, entity: str, data: list):
-        """Update Data
+    def updateRows(self, entity: str, data: list):
+        """Update Rows
         Update rows in a table. The data must be a list of dictionaries that
-        contains the 'idAttribute' and one or more attributes that you wish
-        to update. 
+        contains the 'idAttribute' and must contain values for all attributes
+        in addition to the one that you wish to update. This is ideal for
+        updating rows. To update an attribute, use `updateColumn`.
         
         @param entity (str) : name of the entity to import data into
         @param data (list) : data to import (list of dictionaries)
@@ -144,6 +144,37 @@ class Molgenis(molgenis.Session):
                     label = '{} (batch {})'.format(str(entity), str(d))
                 )
 
+    def updateColumn(self, entity: str, attr: str, data: list):
+        """Update Column
+        Update values of an single column in a table. The data must be a list of
+        dictionaries that contain the `idAttribute` and the value of the
+        attribute that you wish to update. As opposed to the `updateRows`, you
+        do not need to supply values for all columns.
+        
+        @param entity (str) : name of the entity to import data into
+        @param attr (str) : name of the attribute to update
+        @param data (list) : data to import (list of dictionaries)
+        
+        @retrun status message
+        """
+        url = '{}/v2/{}/{}'.format(self._apiUrl, str(entity), str(attr))
+        
+        # single push
+        if len(data) < 1000:
+            self._PUT(url=url, data=data, label=str(entity))
+        
+        # batch push
+        if len(data) >= 1000:
+            for d in range(0, len(data), 1000):
+                self._PUT(
+                    url = url,
+                    data = data[d:d+1000],
+                    label = '{}/{} (batch {})'.format(
+                        str(entity),
+                        str(attr),
+                        str(d)
+                    )
+                )
 
 class cosasLogger:
     def __init__(self):
--- src/python/utils_cosas.py
+++ src/python/utils_cosas.py
@@ -2,10 +2,10 @@
 #' FILE: utils_cosas.py
 #' AUTHOR: David Ruvolo
 #' CREATED: 2021-12-03
-#' MODIFIED: 2022-02-21
+#' MODIFIED: 2022-02-22
 #' PURPOSE: extra functions for COSAS mapping
 #' STATUS: stable
-#' PACKAGES: **see below**
+#' PACKAGES: molgenis.client, datetime, requests, json
 #' COMMENTS: NA
 #'////////////////////////////////////////////////////////////////////////////
 
@@ -103,11 +103,12 @@ class Molgenis(molgenis.Session):
                 )
     
     
-    def updateData(self, entity: str, data: list):
-        """Update Data
+    def updateRows(self, entity: str, data: list):
+        """Update Rows
         Update rows in a table. The data must be a list of dictionaries that
-        contains the 'idAttribute' and one or more attributes that you wish
-        to update. 
+        contains the 'idAttribute' and must contain values for all attributes
+        in addition to the one that you wish to update. This is ideal for
+        updating rows. To update an attribute, use `updateColumn`.
         
         @param entity (str) : name of the entity to import data into
         @param data (list) : data to import (list of dictionaries)
@@ -126,4 +127,36 @@ class Molgenis(molgenis.Session):
                     url = url,
                     data = data[d:d+1000],
                     label = '{} (batch {})'.format(str(entity), str(d))
+                )
+
+    def updateColumn(self, entity: str, attr: str, data: list):
+        """Update Column
+        Update values of an single column in a table. The data must be a list of
+        dictionaries that contain the `idAttribute` and the value of the
+        attribute that you wish to update. As opposed to the `updateRows`, you
+        do not need to supply values for all columns.
+        
+        @param entity (str) : name of the entity to import data into
+        @param attr (str) : name of the attribute to update
+        @param data (list) : data to import (list of dictionaries)
+        
+        @retrun status message
+        """
+        url = '{}/v2/{}/{}'.format(self._apiUrl, str(entity), str(attr))
+        
+        # single push
+        if len(data) < 1000:
+            self._PUT(url=url, data=data, label=str(entity))
+        
+        # batch push
+        if len(data) >= 1000:
+            for d in range(0, len(data), 1000):
+                self._PUT(
+                    url = url,
+                    data = data[d:d+1000],
+                    label = '{}/{} (batch {})'.format(
+                        str(entity),
+                        str(attr),
+                        str(d)
+                    )
                 )
