4f1567c90c6d6aa707883c1ca6366105a5fc8458
davidruvolo51 <davidruvolo51@gmail.com>
2022-04-29 14:41:49 +0200

    minor content revisions; added warnings

--- app/dashboard/src/App.vue
+++ app/dashboard/src/App.vue
@@ -28,58 +28,68 @@
             :value="mostRecentImport.sequencing"
           />
         </DataHighlightContainer>
-        <p class="viz-note">Data was last updated on {{ mostRecentImport.date }}</p>
+        <p class="viz-note">
+          <span v-if="mostRecentImport.date">
+            Data was last updated on {{ mostRecentImport.date }}
+          </span>
+          <span v-else class="text-error">Error: unable to retrieve latest metadata.</span>
+        </p>
       </Section>
-      <Section id="dashboard-cosas-summary" aria-labelledby="dashboard-cosas-summary-title">
-        <div class="col-sm-12 col-md-10">
-          <h2 id="dashboard-cosas-summary-title">Database over the last 30 days</h2>
-          <p>Data is imported into COSAS at regular intervals. The following table displays the change in values over the last 30 days. For more information, see the <a href="/menu/plugins/dataexplorer?entity=cosasreports_imports&hideselect=true">Daily Import</a> table to view the raw data.</p>
-          <DataTable
-            tableId="daily-import-summary"
-            :data="dailyImportSummary"
-            caption="Growth of the COSAS database over the last 30 days"
-            :columnOrder="['category','startingValue','finalValue', 'difference']"
-          />
-        </div>
+      <Section
+        id="dashboard-cosas-summary"
+        class="dashboard-section cosas-data-table"
+        aria-labelledby="dashboard-cosas-summary-title"
+      >
+        <h2 id="dashboard-cosas-summary-title">Database over the last 30 days</h2>
+        <p>Data is imported into COSAS at regular intervals. The following table displays the change in values over the last 30 days. For more information, see the <a href="/menu/plugins/dataexplorer?entity=cosasreports_imports&hideselect=true">Daily Import</a> table to view the raw data.</p>
+        <DataTable
+          tableId="daily-import-summary"
+          :data="dailyImportSummary"
+          caption="Growth of the COSAS database over the last 30 days"
+          :columnOrder="['category','starting.value','final.value', 'difference']"
+        />
       </Section>
-      <Section id="dashboard-coverage-report" aria-labelledby="dashboard-coverage-report-title">
+      <Section
+        id="dashboard-coverage-report"
+        class="dashboard-section cosas-data-table"
+        aria-labelledby="dashboard-coverage-report-title"
+      >
         <h2 id="dashboard-coverage-report-title">COSAS Coverage Report</h2>
-        <p>How much of the COSAS database is used? COSAS has 6 primary tables, 7 secondary tables, and 24 reference tables. To understand how much data is in COSAS, the following tables provide a summary of the primary tables. This includes the columns that are used and how much data is stored in those columns. This data was updated on {{ dateAttributeSummaryDataUpdated }}.</p>
-        <div v-if="attributeSummaryData">
-          <DataTable
-            tableId="subject-attributes"
-            caption="Subject table attributes"
-            :data="attributeSummaryData.subjects"
-            :columnOrder="attributeSummaryColumnOrder"
-          />
-          <DataTable
-            tableId="clinical-attributes"
-            caption="Clinical table attributes"
-            :data="attributeSummaryData.clinical"
-            :columnOrder="attributeSummaryColumnOrder"
-          />
-          <DataTable
-            tableId="samples-attributes"
-            caption="Sample table attributes"
-            :data="attributeSummaryData.samples"
-            :columnOrder="attributeSummaryColumnOrder"
-          />
-          <DataTable
-            tableId="sampleprep-attributes"
-            caption="Sample Preparation table attributes"
-            :data="attributeSummaryData.samplePreparation"
-            :columnOrder="attributeSummaryColumnOrder"
-          />
-          <DataTable
-            tableId="sequencing-attributes"
-            caption="Sequencing table attributes"
-            :data="attributeSummaryData.sequencing"
-            :columnOrder="attributeSummaryColumnOrder"
-          />
-          </div>
+        <p>How much of the COSAS database is used? COSAS has 6 primary tables, 7 secondary tables, and 24 reference tables. To understand how much data is in COSAS, the following tables provide a summary of the primary tables. This includes the columns that are used and how much data is stored in those columns.</p>
+        <p>The data used in the following tables was updated on {{ dateAttributeSummaryDataUpdated }}. If you would like to view the raw data, please follow the link to the <a href="/menu/plugins/dataexplorer?entity=cosasreports_attributesummary&hideselect=true">COSAS Attribute Summary Table</a>.</p>
+        <DataTable
+          tableId="subject-attributes"
+          caption="Summary of subject table attributes"
+          :data="attributeSummaryData.subjects"
+          :columnOrder="attributeSummaryColumnOrder"
+        />
+        <DataTable
+          tableId="clinical-attributes"
+          caption="Summary of clinical table attributes"
+          :data="attributeSummaryData.clinical"
+          :columnOrder="attributeSummaryColumnOrder"
+        />
+        <DataTable
+          tableId="samples-attributes"
+          caption="Summary of sample table attributes"
+          :data="attributeSummaryData.samples"
+          :columnOrder="attributeSummaryColumnOrder"
+        />
+        <DataTable
+          tableId="sampleprep-attributes"
+          caption="Summary of sample preparation table attributes"
+          :data="attributeSummaryData.samplePreparation"
+          :columnOrder="attributeSummaryColumnOrder"
+        />
+        <DataTable
+          tableId="sequencing-attributes"
+          caption="Summary of sequencing table attributes"
+          :data="attributeSummaryData.sequencing"
+          :columnOrder="attributeSummaryColumnOrder"
+        />
       </Section>
     </main>
-    <div id="dashboard-footer" class="bg-dark footer">
+    <div id="dashboard-footer" class="footer">
       <img
         :src="images.logo"
         alt="molgenis open source data platform"
@@ -99,8 +109,12 @@ import DataTable from './components/Table.vue'
 export default {
   data () {
     return {
-      dailyImportSummary: [],
-      mostRecentImport: {},
+      dailyImportSummary: {},
+      mostRecentImport: {
+        subjects: 'NA',
+        samples: 'NA',
+        sequencing: 'NA'
+      },
       attributeSummaryData: {},
       dateAttributeSummaryDataUpdated: null,
       attributeSummaryColumnOrder: [
@@ -160,10 +174,10 @@ export default {
       this.dailyImportSummary = columns.map(column => {
         const row = {
           category: column,
-          startingValue: this.stringAsNumber(dataFiltered.min[0][column]),
-          finalValue: this.stringAsNumber(dataFiltered.max[0][column])
+          'starting.value': this.stringAsNumber(dataFiltered.min[0][column]),
+          'final.value': this.stringAsNumber(dataFiltered.max[0][column])
         }
-        row.difference = row.finalValue - row.startingValue
+        row.difference = row['final.value'] - row['starting.value']
         return row
       })
     },
@@ -218,20 +232,32 @@ export default {
   color: hsl(210, 9%, 47%);
 }
 
+.text-error {
+  color: hsl(12, 100%, 50%);
+  font-weight: 500;
+}
+
 #dashboard {
   color: #3f454b;
 
   .footer {
     padding: 2em 1em;
+    background-color: hsl(218, 98%, 8%);
   }
+
+  .dashboard-section {
+    width: 90%;
+    margin: 0 auto;
+
+    @media screen and (min-width: 972px) {
+      max-width: 1024px;
+    }
+  }
+  
 }
 
 #data-highlights-section {
   background: hsl(0, 0%, 86%);
-}
-
-#viz-section {
-  background: #f6f6f6;
 }
 
 #dashboard-coverage-report-section {
