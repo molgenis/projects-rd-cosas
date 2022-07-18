942be973f872dc7cd6fbd12c4a5f56721392b5d5
davidruvolo51 <davidruvolo51@gmail.com>
2022-07-18 13:37:51 +0200

    added option to specify entity ID

--- app-vue/src/utils/search.js
+++ app-vue/src/utils/search.js
@@ -1,4 +1,21 @@
 
+// Remove Null Object Keys
+// Remove all null keys from an object. This is the first step in preparing
+// filters for a Molgenis DataExplorer URL
+//
+// @param data an object containing one or more Null keys
+//
+// @examples
+// const filters = {
+//   gender: 'female',
+//   age: null,
+//   country: 'Netherlands'
+//   group: null
+// }
+// removeNullObjectKeys(filters)
+// > { gender: 'female', country: 'Netherlands' }
+//
+// @return object
 const removeNullObjectKeys = function (data) {
   const filters = data
   Object.keys(filters).forEach(key => {
@@ -9,6 +26,24 @@ const removeNullObjectKeys = function (data) {
   return filters
 }
 
+// Object To Url Filter Array
+// Object containing  to an array of strings (in Molgenis format). Any value
+// that is a comma-separated string, will be formatted accordingly
+//
+// @param object an object containing one more valid keys
+//
+// @examples
+// const filters = {
+//   gender: 'female',
+//   age: null,
+//   country: 'Australia, New Zealand'
+//   group: null
+// }
+// const f = removeNullObjectKeys(filters)
+// objectToUrlFilterArray(f)
+// > ['gender==female', 'country=in=(Australia,New Zealand)']
+//
+// @return array of strings
 const objectToUrlFilterArray = function (object) {
   const urlFilter = []
   Object.keys(object).forEach(key => {
@@ -27,10 +62,27 @@ const objectToUrlFilterArray = function (object) {
   return urlFilter
 }
 
-const windowReplaceUrl = function (array) {
+// Window Replace Url
+// Open desired database table in a new tab and apply filters
+//
+// @param entity EMX table location as <package>_<entity>
+// @param array an array of filters (i.e., output of objectToUrlFilterArray)
+//
+// @examples
+// const userInputs = {
+//   gender: 'female',
+//   age: null,
+//   country: 'Australia, New Zealand'
+//   group: null
+// }
+// const filters = removeNullObjectKeys(userInputs)
+// const filterArray = objectToUrlFilterArray(filters)
+// windowReplaceUrl('database_table', filterArray)
+
+const windowReplaceUrl = function (entity, array) {
   const filters = array.join(';')
   const filtersEncoded = encodeURIComponent(filters)
-  const baseUrl = '/menu/plugins/dataexplorer?entity=variantdb_variant&mod=data&hideselect=true'
+  const baseUrl = `/menu/plugins/dataexplorer?entity=${entity}&mod=data&hideselect=true`
   const url = baseUrl + '&filter=' + filtersEncoded
   window.open(url, '_blank')
 }
