b6d34c007f772af7ff001bb09c2c20e56e1055f4
davidruvolo51 <davidruvolo51@gmail.com>
2022-07-25 14:31:19 +0200

    finalized search page

--- app-vue/src/pages/HomePageCosas.vue
+++ app-vue/src/pages/HomePageCosas.vue
@@ -7,18 +7,23 @@
       :imageSrc="require('@/assets/header-bg-2.jpg')"
     />
     <main>
-      <Section id="cosas-search-form">
+      <Section id="cosas-search-instructions" aria-labelledby="cosas-search-instructions-title">
+        <h2 id="cosas-search-instructions-title">Instructions</h2>
+        <p>Using the forms below, you can search for files or for patients with a certain phenotype. You may enter one or more values in the search fields (make sure values are separated by a comma) and tick any of the applicable boxes. If there are matching records, the total number of results will be displayed along with a link to view the results. Click the link to view the data in COSAS.</p>
+        <p>If you receive a "Found 0 records" message. This means that no matches could be found based on the provided search criteria. Try adjusting the search parameters. If you continue to receive zero results, then it is possible that the data is not yet available in COSAS.</p>
+        <p>In the event that something goes wrong, an error message will be display. The message will give you the error code, reason, and the URL that was used to search. Please note any error and notify the support desk if the error persists. There are some issues however that are easy to fix. A common issue is a "Bad Request (400)" error. This error occurrs when all search fields and filters are blank. Make sure at least one of the search files isn't blank or one filter has been selected.</p>
+      </Section>
+      <div id="cosas-search" class="cosas-search-container">
         <Form
           id="cosas-files-search-form"
           class="cosas-search-form"
           title="Search for Files"
-          description="Find types of files by one or more patients."
         >
           <FormSection>
             <SearchInput
               id="subjectID"
               label="Search for MDN/UMCG Numbers"
-              description="Enter one ore more MDN/UMCGnr for a patient or a relative of the patient"
+              description="Enter one or more MDN/UMCGnr for a patient or a relative of the patient"
               @search="(value) => fileFilters.belongsToSubject = value"
             />
           </FormSection>
@@ -51,6 +56,7 @@
             :actionHasFailed="fileSearch.hasFailed"
             :actionErrorMessage="fileSearch.errorMessage"
             :actionSuccessMessage="fileSearch.successMessage"
+            :searchResultsUrl="fileSearch.resultsUrl"
           />
         </Form>
         <Form
@@ -75,8 +81,17 @@
             />
           </FormSection>
           <SearchButton id="search-clinical" @click="searchClinical"/>
+          <SearchResultsBox
+            label="Unable to retrieve records"
+            :isPerformingAction="clinicalSearch.isSearching"
+            :actionWasSuccessful="clinicalSearch.wasSuccessful"
+            :actionHasFailed="clinicalSearch.hasFailed"
+            :actionErrorMessage="clinicalSearch.errorMessage"
+            :actionSuccessMessage="clinicalSearch.successMessage"
+            :searchResultsUrl="clinicalSearch.resultsUrl"
+          />
         </Form>
-      </Section>
+      </div>
     </main>
   </Page>
 </template>
@@ -96,8 +111,7 @@ import {
   initSearchResultsObject,
   removeNullObjectKeys,
   objectToUrlFilterArray,
-  setDataExplorerUrl,
-  windowReplaceUrl
+  setDataExplorerUrl
 } from '@/utils/search.js'
 
 export default {
@@ -128,7 +142,8 @@ export default {
         belongsToSubject: null,
         fileFormat: null
       },
-      fileSearch: initSearchResultsObject()
+      fileSearch: initSearchResultsObject(),
+      clinicalSearch: initSearchResultsObject()
     }
   },
   methods: {
@@ -138,6 +153,9 @@ export default {
     resetFileSearch () {
       this.fileSearch = initSearchResultsObject()
     },
+    resetClinicalSearch () {
+      this.clinicalSearch = initSearchResultsObject()
+    },
     searchFiles () {
       this.resetFileSearch()
       this.fileSearch.isSearching = true
@@ -146,43 +164,70 @@ export default {
       const filtersAsUrlParams = objectToUrlFilterArray(filters).join(';')
       const url = `/api/v2/umdm_files?attributes=fileID&q=${filtersAsUrlParams}`
 
-      setTimeout(() => {
-        Promise.all([
-          fetchData(url)
-        ]).then(response => {
-          const data = response[0]
-          this.fileSearch.totalRecordsFound = data.total
-          this.fileSearch.successMessage = `Found ${data.total} result${data.total === 1 ? 's' : ''}`
-          
-          const idsForUrl = data.items.map(row => row.belongsToSubject)
-          const filtersAsUrlParams = objectToUrlFilterArray({ belongsToSubject: idsForUrl.join(',') })
+      Promise.all([
+        fetchData(url)
+      ]).then(response => {
+        const data = response[0]
+        const totalRecordsFound = data.total
+        this.fileSearch.totalRecordsFound = totalRecordsFound
+        this.fileSearch.successMessage = `Found ${totalRecordsFound} result${totalRecordsFound === 1 ? '' : 's'}`
+        this.fileSearch.wasSuccessful = true
+        
+        if (totalRecordsFound > 0) {
+          // this needs to be replaced with fileID
+          const idsForUrl = data.items.map(row => { return row.belongsToSubject.subjectID })
+          const filtersAsUrlParams = objectToUrlFilterArray(
+            { belongsToSubject: idsForUrl.join(',') },
+            { fileFormat: this.fileFilters.fileFormat }
+          )
           const url = setDataExplorerUrl('umdm_files', filtersAsUrlParams)
-          this.fileSearch.url = url
-          
-          this.fileSearch.isSearching = false
-          this.fileSearch.wasSuccesful = true
-        }).catch(error => {
-          this.fileSearch.isSearching = false
-          this.fileSearch.wasSuccesful = false
-          this.fileSearch.hasFailed = true
-          this.fileSearch.errorMessage = error.message
-        })
-      }, 4000)
+          this.fileSearch.resultsUrl = url
+        }
+        
+        this.fileSearch.isSearching = false
+      }).catch(error => {
+        this.fileSearch.isSearching = false
+        this.fileSearch.wasSuccesful = false
+        this.fileSearch.hasFailed = true
+        this.fileSearch.errorMessage = error.message
+      })
     },
     searchClinical () {
+      this.resetClinicalSearch()
+      this.clinicalSearch.isSearching = true
+      
       const filters = removeNullObjectKeys(this.clinicalFilters)
-      const filtersAsUrlParams = objectToUrlFilterArray(filters)
-      const url = setDataExplorerUrl('umdm_clinical', filtersAsUrlParams)
-      windowReplaceUrl(url)
+      const filtersAsUrlParams = objectToUrlFilterArray(filters).join(';')
+      const url = `/api/v2/umdm_clinical?attributes=belongsToSubject&q=${filtersAsUrlParams}`
+
+      Promise.all([
+        fetchData(url)
+      ]).then(response => {
+        const data = response[0]
+        const totalRecordsFound = data.total
+        this.clinicalSearch.totalRecordsFound = totalRecordsFound
+        this.clinicalSearch.successMessage = `Found ${totalRecordsFound} result${totalRecordsFound === 1 ? '' : 's'}`
+        this.clinicalSearch.wasSuccessful = true
+        
+        if (totalRecordsFound > 0) {
+          const idsForUrl = data.items.map(row => { return row.belongsToSubject.subjectID })
+          const filtersAsUrlParams = objectToUrlFilterArray({ belongsToSubject: idsForUrl.join(',') })
+          const url = setDataExplorerUrl('umdm_clinical', filtersAsUrlParams)
+          this.clinicalSearch.resultsUrl = url
+        }
+        this.clinicalSearch.isSearching = false
+      }).catch(error => {
+        this.clinicalSearch.isSearching = false
+        this.clinicalSearch.wasSuccesful = false
+        this.clinicalSearch.hasFailed = true
+        this.clinicalSearch.errorMessage = error.message
+      })
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.cosas-search-form {
-  border-top-color: $green-300;
-}
 
 .form__legend {
   font-size: 12pt;
@@ -208,5 +253,25 @@ export default {
       line-height: 1.4;
     }
   }
+}
+.cosas-search-form {
+  max-width: 462px;
+  border-top-color: $green-300;
+}
+
+#cosas-search {
+  box-sizing: padding-box;
+  padding: 2em 1em;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-start;
+  max-width: 1024px;
+}
+
+#cosas-search-instructions {
+  background-color: $gray-000;
 }
 </style>
