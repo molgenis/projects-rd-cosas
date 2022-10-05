69a936a6be23489a3e3f06e4b1377450ed488ab3
davidruvolo51 <davidruvolo51@gmail.com>
2022-10-05 13:48:22 +0200

    init generic container for checkbox groups

---
+++ app-vue/src/components/CheckboxGroup.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="checkbox__group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'checkbox-group'
+}
+</script>
+
+<style lang="scss" >
+.checkbox__group {
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    input[type="checkbox"] {
+      margin-top: -3px;
+      margin-right: 6px;
+    }
+    label {
+      line-height: 1.4;
+    }
+  }
+}
+</style>
