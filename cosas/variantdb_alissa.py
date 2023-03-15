5bb4c542728ef816b98d84325bfeb84e209fb2c3
davidruvolo51 <davidruvolo51@gmail.com>
2023-03-15 15:29:52 +0100

    finalised script

--- cosas/variantdb_alissa.py
+++ cosas/variantdb_alissa.py
@@ -2,10 +2,10 @@
 # FILE: alissa.py
 # AUTHOR: David Ruvolo
 # CREATED: 2022-04-19
-# MODIFIED: 2023-02-07
+# MODIFIED: 2023-03-15
 # PURPOSE: fetch data from alissa
-# STATUS: experimental
-# PACKAGES: cosas.api.alissa, dotenv, os, requests
+# STATUS: stable
+# PACKAGES: **see below**
 # COMMENTS: The purpose of this script is to extract variant information for
 # all patients, or a subset of patients, from the UMCG instance of Alissa
 # Interpret. To extract classifications, you must run a series of requests.
@@ -27,44 +27,295 @@
 # are available in this python script.
 #//////////////////////////////////////////////////////////////////////////////
 
-from cosas.api.molgenis2 import Molgenis
-from cosas.alissa.client import Alissa
-from dotenv import load_dotenv
-from datetime import datetime
+from oauthlib.oauth2 import LegacyApplicationClient
+from requests_oauthlib import OAuth2Session
 from datatable import dt, f, as_type
-from os import environ
-from tqdm import tqdm
+import molgenis.client as molgenis
+from datetime import datetime
+from os.path import abspath
+import numpy as np
 import requests
+import tempfile
 import json
+import pytz
+import csv
 import re
-load_dotenv()
 
 def now():
   return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
   
 def today():
   return datetime.today().strftime('%Y-%m-%d')
+
+def print2(*args):
+  message = ' '.join(map(str, args))
+  time = datetime.now(tz=pytz.timezone('Europe/Amsterdam')).strftime('%H:%M:%S.%f')[:-3]
+  print(f'[{time}] {message}')
   
 def cleanKeyName(value):
   val = re.sub(r'[()\+]', '', value)
   return re.sub(r'(\s+|[-/])', '_', val)
 
-# Connect to COSAS database
-cosas = Molgenis(environ['MOLGENIS_ACC_HOST'])
-cosas.login(environ['MOLGENIS_ACC_USR'], environ['MOLGENIS_ACC_PWD'])
+class Alissa:
+  """Alissa Interpret Public API (v5.3)"""
+  
+  def __init__(self, host, clientId, clientSecret, username, password):
+    """Create new instance of the client
+    A mini api client to get molecular variant information per patient.
 
+    @param host The url of your Alissa Interpret instance
+    @param clientId provided by Alissa Support
+    @param clientSecret provided by Alissa Support
+    @param username username of the API account
+    @param password password of the API account
+    
+    @reference Alissa Interpret Public API documentation v5.3
+    @return class
+    """
+    self.host=host
+    self.apiUrl=f"{host}/interpret/api/2"
+    self.session=OAuth2Session(client=LegacyApplicationClient(client_id=clientId))
+    self.session.fetch_token(
+      token_url=f"{host}/auth/oauth/token",
+      username=username,
+      password=password,
+      client_id=clientId,
+      client_secret=clientSecret
+    )
+
+    if self.session.access_token:
+      print('Connected to', host, 'as', username)
+    else:
+      print('Unable to connect to', host, 'as', username)
+      
+
+  def _formatOptionalParams(self, params: dict=None) -> dict:
+    """Format Optional Parameters
+    
+    @param params dictionary containg one or more parameter
+    @return dict
+    """
+    return {
+      key: params[key]
+      for key in params.keys()
+      if (key != 'self') and (params[key] is not None)
+    }
+  
+  def _get(self, endpoint, params=None, **kwargs):
+    """GET
+
+    @param endpoint the Alissa Interpret endpoint where data should be
+        sent to. The path "/interpret/api/2" is prefilled.
+    @param params Optional parameters to add to the request
+
+    """
+    uri = f'{self.apiUrl}/{endpoint}'
+    response = self.session.get(uri, params=params, **kwargs)
+    response.raise_for_status()
+    return response.json()
+      
+  def _post(self, endpoint, data=None, json=None, **kwargs):
+    """POST
+    
+    @param endpoint the Alissa Interpret endpoint where data should be
+        sent to. The path "/interpret/api/2" is prefilled.
+    @param data Optional dictionary, list of tuples, bytes, or file-like
+        object
+    @param json Optional json data
+    """
+    uri = f'{self.apiUrl}/{endpoint}'
+    response = self.session.post(uri, data, json, **kwargs)
+    response.raise_for_status()
+    return response.json()
+
+  def getPatientByInternalId(self, patientId: str = None):
+    """Get Patient By ID
+    Retrieve patient information using Alissa's internal identifier for 
+    patients rather than accession number.
+    
+    @param patientId the unique internal identifier of a patient (Alissa ID)
+    @return json
+    """
+    url = f'{self.apiUrl}/patients/{patientId}'
+    response = self.session.get(url)
+    response.raise_for_status()
+    return response.json()
+
+  def getPatients(
+    self,
+    accessionNumber: str = None,
+    createdAfter: str = None,
+    createdBefore: str = None,
+    createdBy: str = None,
+    familyIdentifier: str = None,
+    lastUpdatedAfter: str = None,
+    lastUpdatedBefore: str = None,
+    lastUpdatedBy: str = None
+  ) -> dict:
+    """Get Patients
+    Get all patients. When filter criteria are provided, the result is
+    limited to the patients matching the criteria.
+
+    @param accessionNumber The unique identifier of the patient
+    @param createdAfter Filter patients with a creation date after the
+        specific date time (ISO 8601 date time format)
+    @param createdBefore Filter patients with a creation date before the
+        specific date time (ISO 8601 date time format)
+    @param createdBy User that created the patient
+    @param familyIdentifier The unique identifier of the family.
+    @param lastUpdatedAfter Filter patients with a last updated date after
+        the specified date time (ISO 8601 date time format)
+    @param lastUpdatedBefore Filter patients with a last updated date
+        before the specified date time (ISO 8601 date time format)
+    @param lastUpdatedBy User that updated the patient.
+    
+    @reference Alissa Interpret Public API (v5.3; p21)
+    @return dictionary containing one or more patient records
+    """
+    params = self._formatOptionalParams(params=locals())
+    return self._get(endpoint='patients', params=params)
+
+  def getPatientAnalyses(self, patientId: str) -> dict:
+    """Get Analyses of Patient
+
+    @param patientId The unique internal identifier of the patient
+
+    @reference Alissa Interpret Public API (v5.3; p42)
+    @return dictionary containing metadata for one or more analyses
+    """
+    return self._get(endpoint=f"patients/{patientId}/analyses")
+
+  def getPatientVariantExportId(
+    self,
+    analysisId: int,
+    markedForReview: bool=True,
+    markedIncludeInReport: bool=True
+  ) -> dict:
+    """Request Patient Molecular Variants Export
+    Request an export of all variants. When filter criteria are provided,
+    the result is limited to the variants matching the criteria. By default
+    the parameters `markedForReview` and `markedIncludeInReport` are set to
+    True. This will return all results that have been selected.
+
+    @param analysisId ID of the analysis
+    @param markedForReview Is the variant marked for review
+    @param markedIncludeInReport Is the variant included in the report
+
+    @reference Alissa Interpret Public API (v5.3; p44)
+    @param dictionary containing the export identifier
+    """
+    data={'markedForReview': markedForReview, 'markedIncludeInReport': markedIncludeInReport}
+    api=f"patient_analyses/{analysisId}/molecular_variants/exports"
+    return self._post(endpoint=api,json=data)
+
+  def getPatientVariantExportData(self, analysisId: int, exportId: str) -> dict:
+    """Get Patient Molecular Variants Export
+    Get the exported variants of a patient analysis via the export ID
+    returned when requesting the export.
+
+    @param analysisId The unique internal identifier of an analysis
+    @param exportId The unique internal identifier of the export
+
+    @reference Alissa Interpret Public API (v5.3; p45)
+    @return dictionary containing the molecular variant export data from a
+        single analysis of one patient
+    """
+    api=f"patient_analyses/{analysisId}/molecular_variants/exports/{exportId}"
+    return self._get(endpoint=api)
+
+
+class Molgenis(molgenis.Session):
+  def __init__(self, *args, **kwargs):
+    super(Molgenis, self).__init__(*args, **kwargs)
+    self.fileImportEndpoint = f"{self._root_url}plugin/importwizard/importFile"
+  
+  def _datatableToCsv(self, path, datatable):
+    """To CSV
+    Write datatable object as CSV file
+
+    @param path location to save the file
+    @param data datatable object
+    """
+    data = datatable.to_pandas().replace({np.nan: None})
+    data.to_csv(path, index=False, quoting=csv.QUOTE_ALL)
+  
+  def importDatatableAsCsv(self, pkg_entity: str, data):
+    """Import Datatable As CSV
+    Save a datatable object to as csv file and import into MOLGENIS using the
+    importFile api.
+    
+    @param pkg_entity table identifier in emx format: package_entity
+    @param data a datatable object
+    @param label a description to print (e.g., table name)
+    """
+    with tempfile.TemporaryDirectory() as tmpdir:
+      filepath=f"{tmpdir}/{pkg_entity}.csv"
+      self._datatableToCsv(filepath, data)
+      with open(abspath(filepath),'r') as file:
+        response = self._session.post(
+          url = self.fileImportEndpoint,
+          headers = self._headers.token_header,
+          files = {'file': file},
+          params = {'action': 'add_update_existing', 'metadataAction': 'ignore'}
+        )
+        if (response.status_code // 100 ) != 2:
+          print2('Failed to import data into', pkg_entity, '(', response.status_code, ')')
+        else:
+          print2('Imported data into', pkg_entity)
+        return response
+
+def filterList(data, key, condition):
+  return [row for row in data if row[key] == condition][0]
+
+#///////////////////////////////////////////////////////////////////////////////
+
+# ~ 0 ~
+# Connect
 # Connect to the UMCG instance of Alissa Interpret
 # NOTE: Credentials are stored in the vault, but you will need your own API
 # username and password. This information can be generated if you have been
 # given admin rights in Alissa. Contact agilent support if you have any
 # questions or if you encounter any issues.
 
+# ~ 0a ~
+# For local molgenis development
+# Connect to COSAS database
+# from dotenv import load_dotenv
+# from os import environ
+# load_dotenv()
+
+# cosas = Molgenis(environ['MOLGENIS_ACC_HOST'])
+# cosas.login(environ['MOLGENIS_ACC_USR'], environ['MOLGENIS_ACC_PWD'])
+
+# alissa = Alissa(
+#   host=environ['ALISSA_HOST'],
+#   clientId=environ['ALISSA_CLIENT_ID'],
+#   clientSecret=environ['ALISSA_CLIENT_SECRET'],
+#   username=environ['ALISSA_API_USR'],
+#   password=environ['ALISSA_API_PWD']
+# )
+
+# ~ PROD ~
+cosas = Molgenis('http://localhost/api/', token='${molgenisToken}')
+
+credentials = cosas.get(
+  'sys_sec_Token',
+  q='description=like="alissa-api-"',
+  attributes='token,description'
+)
+
+host=filterList(credentials,'description','alissa-api-host')['token']
+clientId=filterList(credentials,'description','alissa-api-client-id')['token']
+clientSecret=filterList(credentials,'description', 'alissa-api-client-secret')['token']
+apiUser=filterList(credentials,'description','alissa-api-username')['token']
+apiPwd=filterList(credentials,'description','alissa-api-password')['token']
+
 alissa = Alissa(
-  host=environ['ALISSA_HOST'],
-  clientId=environ['ALISSA_CLIENT_ID'],
-  clientSecret=environ['ALISSA_CLIENT_SECRET'],
-  username=environ['ALISSA_API_USR'],
-  password=environ['ALISSA_API_PWD']
+  host=host,
+  clientId=clientId,
+  clientSecret=clientSecret,
+  username=apiUser,
+  password=apiPwd
 )
 
 #///////////////////////////////////////////////////////////////////////////////
@@ -180,7 +431,7 @@ for variantRow in tqdm(variantExportsByAnalyses):
     pass
 
 # save to file so you don't have to rerun the script
-# with open('private/alissa_variant_export_20220206.json', 'w') as file:
+# with open('private/alissa_variantexport.json', 'w') as file:
 #   json.dump(variantExport,file)
 
 #///////////////////////////////////////////////////////////////////////////////
@@ -216,23 +467,35 @@ for row in tqdm(variantExport):
   # process results
   else:
     newRow = row
+    
+    if bool(newRow['variantAssessment']):
+      newRow['variantAssessment'] = json.dumps(newRow['variantAssessment'])
+    
     if not bool(newRow['variantAssessmentLabels']):
       newRow['variantAssessmentLabels'] = None
     
     if not bool(newRow['variantAssessmentNotes']):
       newRow['variantAssessmentNotes'] = None
+      
+    if bool(newRow.get('geneProfileReport')):
+      newRow['geneProfileReport'] = json.dumps(newRow['geneProfileReport'])
     
     # flatten databaseReferences
     if 'databaseReferences' in newRow:
       if bool(newRow['databaseReferences']):
         for key in newRow['databaseReferences'].keys():
-          newRow[f'databaseReferences_{key}'] = newRow['databaseReferences'][key]
+          if bool(newRow['databaseReferences'][key]):
+            newRow[f'databaseReferences_{key}'] = newRow['databaseReferences'][key]
+          else:
+            newRow[f'databaseReferences_{key}'] = None
         del newRow['databaseReferences']
     
     # flatten classification tree labels
     if 'classificationTreeLabelsScore' in newRow:
       if bool(newRow['classificationTreeLabelsScore']):
-        newRow['classificationTreeLabels'] = newRow['classificationTreeLabelsScore']['labels']
+        newRow['classificationTreeLabels'] = json.dumps(
+          newRow['classificationTreeLabelsScore']['labels']
+        )
         newRow['classificationTreeScores'] = newRow['classificationTreeLabelsScore']['score']
         del newRow['classificationTreeLabelsScore']
       
@@ -240,13 +503,18 @@ for row in tqdm(variantExport):
     if 'customFields' in newRow:
       if bool(newRow['customFields']):
         newRow['customFields'] = json.dumps(newRow['customFields'])
+      else:
+        newRow['customFields'] = None
     
     # flatten externalDatabases
     if 'externalDatabases' in newRow:
       if bool(newRow['externalDatabases']):
         for key in newRow['externalDatabases'].keys():
           newKey = cleanKeyName(key)
-          newRow[f'_{newKey}'] = newRow['externalDatabases'][key]
+          if bool(newRow['externalDatabases'][key]):
+            newRow[f'_{newKey}'] = json.dumps(newRow['externalDatabases'][key])
+          else:
+            newRow[f'_{newKey}'] = None
         del newRow['externalDatabases']
     
     # flatten platformDatasets
@@ -254,8 +522,15 @@ for row in tqdm(variantExport):
       if bool(newRow['platformDatasets']):
         for key in newRow['platformDatasets'].keys():
           newKey = cleanKeyName(key)
-          newRow[f'_{newKey}'] = newRow['platformDatasets'][key]
+          if bool(newRow['platformDatasets'][key]):
+            newRow[f'_{newKey}'] = json.dumps(newRow['platformDatasets'][key])
+          else:
+            newRow[f'_{newKey}'] = None
         del newRow['platformDatasets']
+  
+    for column in newRow.keys():
+      if bool(newRow[column]) and type(newRow[column]) == str:
+        newRow[column] = newRow[column].replace(',',';')
   
     # set api records
     newRow['hasError'] = False
@@ -284,7 +559,7 @@ for id in tqdm(variantPatientIDs):
     variantsDT[f.patientId==id,'umcgNr'] = refRow['umcgNr'].to_list()[0]
     variantsDT[f.patientId==id,'accessionNumber'] = refRow['accessionNr'].to_list()[0]
   else:
-    raise SystemError(f'Error: {id} not found in patients dataset')
+    raise SystemError(f'Error: {id} not found in patients dataset')  
 
 # ~ 6b ~
 # merge analysis info
@@ -296,13 +571,11 @@ for row in analysesByPatient:
       row['targetPanelNames'] = ','.join(row['targetPanelNames'])
 analysisDT = dt.Frame(analysesByPatient)
 
-
 analysisDT[:, dt.update(analysisId = as_type(f.analysisId, str))]
 variantsDT[:, dt.update(analysisId = as_type(f.analysisId, str))]
 
 analysisIDs = analysisDT['analysisId'].to_list()[0]
 variantAnalysisIDs = variantsDT['analysisId'].to_list()[0]
-
 
 for id in tqdm(variantAnalysisIDs):
   if id in analysisIDs:
@@ -320,8 +593,19 @@ for id in tqdm(variantAnalysisIDs):
 
 variantsDT['dateRetrieved'] = today()
 
-# import 
+
+# variantsDT.to_csv('private/alissa_variantexport.csv')
+#///////////////////////////////////////////////////////////////////////////////
+
+# ~ 7 ~
+# Import data
 # cosas.delete('alissa_variantexports')
+
+cosas.importDatatableAsCsv(
+  pkg_entity='alissa_patients',
+  data = patientsDT
+)
+
 cosas.importDatatableAsCsv(
   pkg_entity='alissa_variantexports',
   data = variantsDT
