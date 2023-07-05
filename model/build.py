aff5d17a87302f167dbe7abc5e2fb4f13be94bfc
davidruvolo51 <davidruvolo51@gmail.com>
2023-07-05 13:51:28 +0200

    added data sources table to reports emx

--- model/build.py
+++ model/build.py
@@ -2,7 +2,7 @@
 # FILE: build.py
 # AUTHOR: David Ruvolo
 # CREATED: 2022-11-09
-# MODIFIED: 2022-11-09
+# MODIFIED: 2023-07-05
 # PURPOSE: generic build script for EMX models
 # STATUS: stable
 # PACKAGES: yamlemxconvert
@@ -23,3 +23,14 @@ alissa = Convert(files = ['model/alissa.yaml'])
 alissa.convert()
 alissa.compileSemanticTags()
 alissa.write(name='alissa', outDir='dist')
+
+
+# report reports
+reports = Convert(files = [
+  'model/cosasreports.yaml',
+  'model/cosasreports_refs.yaml'
+])
+
+reports.convert()
+reports.compileSemanticTags()
+reports.write(name="cosasreports", outDir="dist")
